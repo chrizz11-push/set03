0d3ba2e6af9e5050ef2a588d271f63a79b7cec48
chrizz11-push <christopherchima11@gmail.com>
2021-08-16 14:30:32 +0100

    now

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (req,res) => {
+     res.write("server is ready")
+     res.end();
+})
+
+app.listen(port, () => {
+    console.log(`server is listening to:${port}`);
+})
